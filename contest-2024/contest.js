54d7720d57203768751d883edb1c688cb1a47859
rimu-tem <rinrinppcow@gmail.com>
2024-12-20 14:01:30 +0900

    contest.js を編集

--- contest-2024/contest.js
+++ contest-2024/contest.js
@@ -14,5 +14,27 @@ serchButton.addEventListener(
     if ((people.length === 0) || (startStation.length === 0) || (hotelState.length === 0)) {
       return;
     }
+
+    resultDivision.innerText = '';
+    const heading = document.createElement('h3');
+    heading.innerText = '計算結果';
+    resultDivision.appendChild(heading);
+
+    const paragraph = document.createElement('p');
+    const result = serch(people);
+    paragraph.innerText = result;
+    resultDivision.appendChild(paragraph);
   }
-)
+
+);
+
+const answers = [
+  '遠征費は総額###money###円です。そのうち交通費は###station###円、宿泊費は###hotel###円です。'
+]
+
+function serch(people) {
+  function station(startStation) {
+    
+
+  }
+}

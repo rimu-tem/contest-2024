93dcd65ca7230d29a52e7789f5fa42352d25b7c7
rimu-tem <rinrinppcow@gmail.com>
2024-12-20 21:40:29 +0900

    全体完成（未調整）

--- contest-2024/contest.js
+++ contest-2024/contest.js
@@ -1,19 +1,33 @@
 'use strict';
 const peopleInput = document.getElementById('people');
-const startStationInput = document.getElementById('startStation');
-const hotelStateInput = document.getElementById('hotelState');
+const startStationSelect = document.getElementById('startStation');
+const hotelLankSelect= document.getElementById('hotelLank');
+const hotelDateInput = document.getElementById('hotelDate');
 const resultDivision = document.getElementById('result-area');
 const serchButton = document.getElementById('serch');
+
 
 serchButton.addEventListener(
   'click',
   () => {
     const people = peopleInput.value;
-    const startStation = startStationInput.value;
-    const hotelState = hotelStateInput.value;
-    if ((people.length === 0) || (startStation.length === 0) || (hotelState.length === 0)) {
+    startStationSelect.onchange = function(){
+      let startStation = this.value;
+      console.log(startStation);
+    }
+    hotelLankSelect.onchange = function(){
+      let hotelLank = this.value;
+      console.log(hotelLank);
+    }
+    const hotelDate = hotelDateInput.value;
+    if ((people.length === 0) || (startStation.length === 0) || (hotelLank.length === 0) || (hotelDate.length === 0)) {
       return;
     }
+    
+
+
+
+
 
     resultDivision.innerText = '';
     const heading = document.createElement('h3');
@@ -29,12 +43,125 @@ serchButton.addEventListener(
 );
 
 const answers = [
-  '遠征費は総額###money###円です。そのうち交通費は###station###円、宿泊費は###hotel###円です。'
+  '遠征費は総額###money###円です。そのうち交通費は###station###円、宿泊費は###hotel###円、食費は###meal###円です。'
 ]
 
-function serch(people) {
-  function station(startStation) {
-    
 
+function serch(people) {
+  let result = answers[0];
+  function station(startStation) {
+    if (startStation == '宇都宮駅') {
+      let smoney = 1980 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '小山駅') {
+      let smoney = 1518 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '栃木駅') {
+      let smoney = 1694 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '那須塩原駅') {
+      let smoney = 2640 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '佐野駅') {
+      let smoney = 1980 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '石橋駅') {
+      let smoney = 1694 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '雀宮駅') {
+      let smoney = 1980 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '東武宇都宮駅') {
+      let smoney = 1434 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '野木駅') {
+      let smoney = 1166 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '間々田駅') {
+      let smoney = 1342 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '小金井駅') {
+      let smoney = 1518 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '西那須野駅') {
+      let smoney = 2640 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '自治医大駅') {
+      let smoney = 1694 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '氏家駅') {
+      let smoney = 2310 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '矢板駅') {
+      let smoney = 2640 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '足利市駅') {
+      let smoney = 1992 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '岡本駅') {
+      let smoney = 1980 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '黒磯駅') {
+      let smoney = 3080 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }else if (startStation == '宝積寺駅') {
+      let smoney = 2310 * people * 2;
+      return smoney;
+      //result = result.replaceAll('###station###', smoney);
+    }
   }
+  function hotel(hotelLank) {
+    if (hotelLank == '５つ星ホテル（1泊50000円）') {
+      let hmoney = 50000 * people * hotelDate;
+      return hmoney;
+      //result = result.replaceAll('###hotel###', hmoney);
+    }else if (hotelLank == '４つ星ホテル（1泊35000円）') {
+      let hmoney = 35000 * people * hotelDate;
+      return hmoney;
+      //result = result.replaceAll('###hotel###', hmoney);
+    }else if (hotelLank == '３つ星ホテル（1泊15000円）') {
+      let hmoney = 15000 * people * hotelDate;
+      return hmoney;
+      //result = result.replaceAll('###hotel###', hmoney);
+    }else if (hotelLank == 'ビジネスホテル（1泊7500円）') {
+      let hmoney = 7500 * people * hotelDate;
+      return hmoney;
+      //result = result.replaceAll('###hotel###', hmoney);
+    }
+  }
+  function meal(hotelDate) {
+    let mmoney = 6000 * people * hotelDate
+    return mmoney;
+    //result = result.replaceAll('###meal###', mmoney);
+  }
+
+
+  const smoney = station(startStation);
+  const hmoney = hotel(hotelLank);
+  const mmoney = meal(hotelDate);
+  let allmoney = smoney + hmoney + mmoney;
+  let result1 = result.replaceAll('###money###', allmoney);
+  let result2 = result1.replaceAll('###station###', smoney);
+  let result3 = result2.replaceAll('###hotel###', hmoney);
+  let result4 = result3.replaceAll('###meal###', mmoney);
+
+  return result4;
 }
